92f41897a91ee6326a5fae0daba31e2acaff2065
aml19 <kassaouiamel2019@gmail.com>
2026-09-18 21:37:09 +0100

    done

---
+++ app.js
@@ -0,0 +1,25 @@
+// ===== VARIABLES =====
+
+const player0 = document.querySelector(".player-0");
+const player1 = document.querySelector(".player-1");
+
+const score0 = document.querySelector(".score-0");
+const score1 = document.querySelector(".score-1");
+
+const currentScore0 = document.querySelector(".current-score-0");
+const currentScore1 = document.querySelector(".current-score-1");
+
+const dice = document.querySelector("img");
+
+const roll = document.querySelector(".roll");
+const hold = document.querySelector(".hold");
+const newGame = document.querySelector(".new-game");
+
+
+
+let scores = [43, 24];
+let currentScore = 0;
+let activePlayer = 0;
+
+
+
